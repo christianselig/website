dffd3e680a6cb0336c471e92637895e129379895
Christian Selig <me@christianselig.com>
2013-09-23 17:24:08 -0300

    Added smooth scrolling to work link.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,15 @@ jQuery(document).ready(function($) {
 		}
 	});
 
+	var $root = $('html, body');
+
+	$('a').click(function() {
+	    $root.animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+
 	$(".contact input[type='submit']").click(function(event) {
 		event.preventDefault();
 
